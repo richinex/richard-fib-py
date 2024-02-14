42c71f13c0db2de1e1ecf55e9c7c68726e39e788
Richard Chukwu <richinex@gmail.com>
2024-02-14 08:13:23 +0100

    added fib_number.py and fib_numbers.py updated

--- richard_fib_py/fib_calcs/fib_number.py
+++ richard_fib_py/fib_calcs/fib_number.py
@@ -1,8 +1,14 @@
-from typing import Optional
-def recurring_fibonacci_number(number: int) -> Optional[int]:
+def recurring_fibonacci_number(number: int) -> int:
+    """
+    Calculates the fibonacci number needed.
+
+    :param number: (int) the Fibonacci number to be calculated
+    :return: (Optional[int]) the calculated fibonacci number
+    """
     if number < 0:
-        return None
+        raise ValueError("Fibonacci has to be equal or above zero")
     elif number <= 1:
         return number
     else:
-        return recurring_fibonacci_number(number - 1) + recurring_fibonacci_number(number - 2)
+        return recurring_fibonacci_number(number - 1) + \
+               recurring_fibonacci_number(number - 2)
--- richard_fib_py/fib_calcs/fib_numbers.py
+++ richard_fib_py/fib_calcs/fib_numbers.py
@@ -1,5 +1,13 @@
 from typing import List
-from .fib_numbers import recurring_fibonacci_number
+
+from .fib_number import recurring_fibonacci_number
+
 
 def calculate_numbers(numbers: List[int]) -> List[int]:
-    return [recurring_fibonacci_number(number) for number in numbers]
+    """
+    Calculates a range of Fibonacci numbers from a list.
+
+    :param numbers: (List[int]) the Fibonacci numbers to be calculated
+    :return: (List[int]) the calculated Fibonacci numbers
+    """
+    return [recurring_fibonacci_number(number=i) for i in numbers]
